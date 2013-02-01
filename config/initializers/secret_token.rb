f5d28936eaae92fb017cde41e4abe228aa37e2e1
Gervel Giva <gervelgiva@gmail.com>
2013-02-01 13:42:45 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nlvpms::Application.config.secret_token = 'eb1a578d4faef91fcfc7246e404d67e4cb99915d6af906e838a59b2af9d31d55d606fa11e0e49214cdc9b882a32f65212ae30980c2901f370e2cb330a815c531'
